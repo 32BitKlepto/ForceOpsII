2eaaf7718ed517026207b4e349fc712684c4cfdd
32BitKlepto <90978686+32BitKlepto@users.noreply.github.com>
2021-09-18 12:04:56 -0500

    Add files via upload

---
+++ BlackCox2/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace BlackCox2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroToggle1 = new MetroFramework.Controls.MetroToggle();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(93, 127);
+            this.metroButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(87, 22);
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = "Connect";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(93, 179);
+            this.metroButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(87, 22);
+            this.metroButton2.TabIndex = 1;
+            this.metroButton2.Text = "Exit";
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroToggle1
+            // 
+            this.metroToggle1.AutoSize = true;
+            this.metroToggle1.Location = new System.Drawing.Point(93, 97);
+            this.metroToggle1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.metroToggle1.Name = "metroToggle1";
+            this.metroToggle1.Size = new System.Drawing.Size(80, 17);
+            this.metroToggle1.TabIndex = 2;
+            this.metroToggle1.Text = "Off";
+            this.metroToggle1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroToggle1.UseVisualStyleBackColor = true;
+            this.metroToggle1.CheckedChanged += new System.EventHandler(this.metroToggle1_CheckedChanged);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Small;
+            this.metroLabel1.Location = new System.Drawing.Point(71, 49);
+            this.metroLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(128, 15);
+            this.metroLabel1.TabIndex = 3;
+            this.metroLabel1.Text = "Created by Klepto#4867";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Small;
+            this.metroLabel2.Location = new System.Drawing.Point(104, 74);
+            this.metroLabel2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(62, 15);
+            this.metroLabel2.TabIndex = 4;
+            this.metroLabel2.Text = "Force Host";
+            this.metroLabel2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(93, 153);
+            this.metroButton3.Margin = new System.Windows.Forms.Padding(2);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(87, 22);
+            this.metroButton3.TabIndex = 5;
+            this.metroButton3.Text = "Inject Jiggy";
+            this.metroButton3.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 219);
+            this.ControlBox = false;
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroToggle1);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(13, 60, 13, 13);
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Orange;
+            this.Text = "Force Ops II";
+            this.TextAlign = System.Windows.Forms.VisualStyles.HorizontalAlign.Center;
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroToggle metroToggle1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+    }
+}
+
---
+++ BlackCox2/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using JRPC_Client;
+using MetroFramework;
+using MetroFramework.Forms;
+using XDevkit;
+
+namespace BlackCox2
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        IXboxConsole Console;
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            if (Console.Connect(out Console))
+            {
+                metroLabel1.Text = "Connected to Console!";
+            }
+            else
+            {
+                metroLabel1.Text = "Error: Could not connect.";
+            }
+            JRPC.XNotify(Console, "Force Ops II Connected! Enjoy :) -Klepto");
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public uint BO2CMD = 2185237984;
+        public uint BO2_StatEntry = 2215380224;
+
+        private void metroToggle1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (metroToggle1.Checked == false) // If off turn on
+            {
+                if (!JRPC.Connect(this.Console, out this.Console, "default"))
+                {
+                    metroLabel1.Text = "Connect console first!";
+                    return;
+                }
+                JRPC.CallVoid(this.Console, this.BO2CMD, new object[2]
+                {
+                    (object) 0,
+                    (object) "party_connectToOthers 01; partyMigrate_disabled 00"
+                });
+            }
+            else if (metroToggle1.Checked == true) // If on turn off
+            {
+                if (!JRPC.Connect(this.Console, out this.Console, "default"))
+                {
+                    metroLabel1.Text = "Connect console first!";
+                    return;
+                }
+                JRPC.CallVoid(this.Console, this.BO2CMD, new object[2]
+                {
+                    (object) 0,
+                    (object) "party_connectToOthers 00; partyMigrate_disabled 01; sv_endGameIfISuck 0; badhost_endgameifisuck 0\x200B; set party_gamestarttimelength 2; set party_pregamestarttimerlength 2; set party_timer 25"
+                });
+            }
+        }
+
+        public uint GET_ALIGNED_DWORD(uint x)
+        {
+            return (uint)((int)x + 3 & -4);
+        }
+
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            string startupPath = Application.StartupPath;
+            uint num1 = 1076887552;
+            uint num2;
+            if (!JRPC.Connect(this.Console, out this.Console, "default"))
+            {
+                metroLabel1.Text = "Connect console first!";
+                return;
+            }
+            else try
+            {
+                uint alignedDword1 = this.GET_ALIGNED_DWORD(num1 + 1U);
+                byte[] numArray1 = File.ReadAllBytes(startupPath + "\\GSC\\Jiggy4.5\\maps\\mp\\_ambientpackage.gsc");
+                JRPC.SetMemory(this.Console, 2199829440U, ((IEnumerable<byte>)BitConverter.GetBytes(alignedDword1)).Reverse<byte>().ToArray<byte>());
+                JRPC.SetMemory(this.Console, alignedDword1, new byte[numArray1.Length + 1]);
+                JRPC.SetMemory(this.Console, alignedDword1, numArray1);
+                uint alignedDword2 = this.GET_ALIGNED_DWORD(alignedDword1 + (uint)(numArray1.Length + 1) + 1U);
+                byte[] numArray2 = File.ReadAllBytes(startupPath + "\\GSC\\Jiggy4.5\\maps\\mp\\_development_dvars.gsc");
+                JRPC.SetMemory(this.Console, 2199829632U, ((IEnumerable<byte>)BitConverter.GetBytes(alignedDword2)).Reverse<byte>().ToArray<byte>());
+                JRPC.SetMemory(this.Console, alignedDword2, new byte[numArray2.Length + 1]);
+                JRPC.SetMemory(this.Console, alignedDword2, numArray2);
+                uint alignedDword3 = this.GET_ALIGNED_DWORD(alignedDword2 + (uint)(numArray2.Length + 1) + 1U);
+                byte[] numArray3 = File.ReadAllBytes(startupPath + "\\GSC\\Jiggy4.5\\maps\\mp\\_sticky_grenade.gsc");
+                JRPC.SetMemory(this.Console, 2199829980U, ((IEnumerable<byte>)BitConverter.GetBytes(alignedDword3)).Reverse<byte>().ToArray<byte>());
+                JRPC.SetMemory(this.Console, alignedDword3, new byte[numArray3.Length + 1]);
+                JRPC.SetMemory(this.Console, alignedDword3, numArray3);
+                uint alignedDword4 = this.GET_ALIGNED_DWORD(alignedDword3 + (uint)(numArray3.Length + 1) + 1U);
+                byte[] numArray4 = File.ReadAllBytes(startupPath + "\\GSC\\Jiggy4.5\\maps\\mp\\gametypes\\_rank.gsc");
+                JRPC.SetMemory(this.Console, 2199830484U, ((IEnumerable<byte>)BitConverter.GetBytes(alignedDword4)).Reverse<byte>().ToArray<byte>());
+                JRPC.SetMemory(this.Console, alignedDword4, new byte[numArray4.Length + 1]);
+                JRPC.SetMemory(this.Console, alignedDword4, numArray4);
+                num2 = alignedDword4 + (uint)(numArray4.Length + 1);
+                    metroLabel1.Text = "Jiggy Menu Injected!";
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Failed To Inject Jiggy V4.5");
+            }
+        }
+    }
+}
